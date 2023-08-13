be0a5f72044621fef8cc4ed530551c4249e1a0c0
Giovane Santos <giovane98sp@gmail.com>
2023-08-13 18:19:23 -0300

    add clearForm function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
 import './Button.css'
 
-export const Button = ({id, text}) => {
+export const Button = ({id, text, action}) => {
+  const handleAction = (e) => {
+    action(e)
+  }
+
   return (
-    <button id={id}>{text}</button>
+    <button id={id} onClick={handleAction} >{text}</button>
   )
 }
--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import { Button } from './Button';
 import './ImcCalc.css';
 
 export const ImcCalc = () => {
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+
+  const clearForm = (e) => {
+    e.preventDefault();
+    setHeight('');
+    setWeight('');
+  };
+
   return (
     <div id="calc-container">
       <h2>Calculadora de IMC</h2>
@@ -9,16 +19,30 @@ export const ImcCalc = () => {
         <div className="form-inputs">
           <div className="form-control">
             <label htmlFor="height">Altura:</label>
-            <input type="text" id="height" name="height" placeholder="Exemplo 1,75" />
+            <input
+              type="text"
+              id="height"
+              name="height"
+              placeholder="Exemplo 1,75"
+              onChange={(e) => setHeight(e.target.value)}
+              value={height}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="weight">Peso:</label>
-            <input type="text" id="weight" name="weight" placeholder="Exemplo 70,5" />
+            <input
+              type="text"
+              id="weight"
+              name="weight"
+              placeholder="Exemplo 70,5"
+              onChange={(e) => setWeight(e.target.value)}
+              value={weight}
+            />
           </div>
         </div>
         <div className="action-control">
-          <Button id="calc-btn" text='calcular' />
-          <Button id="clear-btn" text='Limpar' />
+          <Button id="calc-btn" text="calcular" />
+          <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
     </div>
